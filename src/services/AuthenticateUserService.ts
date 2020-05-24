06592c4fae9821fc43d031e620f9dad8049e3b9a
cleberigd <cleber.carvalho@ignicaodigital.com.br>
2020-05-24 13:12:50 -0300

    refactor: separe token configurations

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,6 +3,7 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
+import authConfig from '../config/auth';
 
 interface Request {
   email: string;
@@ -27,9 +28,11 @@ class AuthenticateUserService {
     if (!passwordMatched)
       throw new Error('Incorret email/password combination.');
 
-    const token = sign({}, '1aa6c8cabbf867113319e84cce65b6e6', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
