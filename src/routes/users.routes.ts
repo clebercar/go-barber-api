a4a09cf36a5b5b55562ce806a33aeb5f4e3772e2
cleberigd <cleber.carvalho@ignicaodigital.com.br>
2020-05-24 12:47:28 -0300

    chore: route create users

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import CreateserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateserService();
+    const user = await createUser.execute({ name, email, password });
+
+    delete user.password;
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default usersRouter;
