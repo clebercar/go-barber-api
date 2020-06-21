848f5e708b7ebe1389be14da700271f64e046772
cleber <contatoclebercarvalho@gmail.com>
2020-06-20 23:38:07 -0300

    chore: add emoji in text that informs you that the server is up

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -33,5 +33,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 });
 
 app.listen(3333, () => {
-  console.log('Server started');
+  console.log('⚡ Server started ⚡');
 });
