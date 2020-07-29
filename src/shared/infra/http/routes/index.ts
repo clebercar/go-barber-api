ebbcfdaaa551e7d96ff1b8ae455432ac3843b738
cleber <contatoclebercarvalho@gmail.com>
2020-07-29 20:17:51 -0300

    chore: create routes to handle all password actions

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
-import sessionsRouter from '@modules/users/infra/http/routes//sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/passwords.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/appointments', appointmentsRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
