78340283ecf7a7947264b69eeea9bd9b111d9a51
cleber <contatoclebercarvalho@gmail.com>
2020-07-29 20:23:40 -0300

    refactor: remove duplicate bar

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/passwords.routes';
 
 const routes = Router();
