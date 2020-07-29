be0b31a81a28f3ae6cd0df505a5afa8d46ccb68d
cleber <contatoclebercarvalho@gmail.com>
2020-07-28 22:07:44 -0300

    refactor: throw error when user does not exists

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 
-// import AppError from '@shared/errors/AppError';
-// import User from '../infra/typeorm/entities/User';
+import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -19,6 +18,9 @@ class SendForgotPasswordEmailService {
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) throw new AppError('User does not exists.');
     this.mailProvider.sendMail(
       email,
       'Pedido de recuperação de senha recebido.',
